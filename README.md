e1704adab7960f3b95e65dfba0632250da88c452
XeoPlay <xeoplay123456@gmail.com>
2020-06-12 19:31:43 +0300

    version 0.0.3

---
+++ README.md
@@ -0,0 +1,28 @@
+# xeo-simple-calc
+This is my first python library. It is a simple calculator.
+
+## How to use
+First, install the library
+```python
+pip install xeo-simple-calc==0.0.3
+```
+Next, import it
+```python
+from xeosimplecalc import *
+```
+## Commands
+```python
+add(a,b)
+subtract(a,b)
+multiply(a,b)
+float_divide(a,b)
+int_divide(a,b)
+```
+```python
+float_divide()
+``` 
+returns a float
+```python
+int_divide()
+``` 
+returns an int
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-with open("README.txt", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 with open("CHANGELOG.txt", "r") as CHANGELOG:
     changes = CHANGELOG.read()
 
 setuptools.setup(
     name="xeo_simple_calc",
-    version="0.0.2",
-    author="Grigoriy Agasiyants",
+    version="0.0.3",
+    author="XeoPlay",
     author_email="xeoplay123456@gmail.com",
     description="A small basic calculator",
     long_description=long_description + "\n\n" + changes,
-    url="",
+    url="https://github.com/XeoPlay/xeo-simple-calc",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
